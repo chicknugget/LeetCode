95926be7e5e5cab307fe9e0394a5c24ee1c3dddf
Shreya Chaudhuri <shreyachaudhri4@gmail.com>
2025-07-31 12:16:25 +0530

    Time: 90 ms (5.23%) | Memory: 20.2 MB (95.14%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        self.nums=nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+        
+        
